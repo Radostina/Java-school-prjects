83160a4ef26ccbab38ef01bc224e6e9e86ff6aab
Radostina <radi.chipanova@gmail.com>
2020-09-28 14:04:49 +0300

    Update Casting02.java

--- Casting02.java
+++ Casting02.java
@@ -20,7 +20,7 @@ public class Casting02 {
         //The current population of Asia is an estimated 60% of the world population
         double percentAsia = 0.6;
         long currentAsiaPop = (long)(currentWorldPop * percentAsia);
-        System.out.println("Current Asia Population: " +currentAsiaPop);      
+        //System.out.println("Current Asia Population: " +currentAsiaPop);      
        
     }   
 }
